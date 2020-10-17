12e7f2e1e54f45152a27f6bf5417e7e9581be2b3
Sveta2507 <dnsdns.dn@googlemail.com>
2020-10-17 13:00:22 +0300

    work

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import css from "./css/style.css";
+
+import timeRefs from "./refs/timeRefs.js";
+console.log(timeRefs);
+
+const newYearTime = new Date("Jan 1 2021");
+console.log(+newYearTime);
+
+const startTime = Date.now();
+console.log(startTime);
+
+let currentTime = 0;
+let timeBetween = 0;
+
+function getTimeBetween() {
+  currentTime = Date.now();
+  timeBetween = +newYearTime - currentTime;
+  let daysBefore = Math.floor(timeBetween / 1000 / 60 / 60 / 24);
+  let hoursBefore = Math.floor(
+    (timeBetween % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+  let minutesBefore = Math.floor(
+    (timeBetween % (1000 * 60 * 60)) / (1000 * 60),
+  );
+  let secondsBefore = Math.floor((timeBetween % (1000 * 60)) / 1000);
+
+  console.log("timeBetween", timeBetween);
+
+  timerRefs.daysContent.textContent = daysBefore;
+  timerRefs.hoursContent.textContent = hoursBefore;
+  timerRefs.minsContent.textContent = minutesBefore;
+  timerRefs.secondsContent.textContent = secondsBefore;
+}
+
+let interval = setInterval(getTimeBetween, 1000);
