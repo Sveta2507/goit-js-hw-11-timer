7f3283c7a7aa60300e1d2956bd9d301a99167d82
Sveta2507 <dnsdns.dn@googlemail.com>
2020-10-31 19:26:52 +0200

    ready

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import css from "./css/style.css";
 
-import timeRefs from "./refs/timeRefs.js";
-console.log(timeRefs);
+import timerRefs from "./refs/timerRefs.js";
+console.log(timerRefs);
 
 const newYearTime = new Date("Jan 1 2021");
 console.log(+newYearTime);
@@ -33,3 +33,27 @@ function getTimeBetween() {
 }
 
 let interval = setInterval(getTimeBetween, 1000);
+
+// import css from "./css/style.css";
+// import timerRefs from './refs/timerRefs.js'
+
+// let myDate = Jan 1 2021
+
+// function getTimeBetween(date){
+//   let newYearTime = new Date(date)
+//   let timeBetween = +newYearTime - Date.now()
+//   //       ms = ms          /  s    / min / hours /
+//   let daysBefore = Math.floor(timeBetween / 1000 / 60 / 60 / 24)
+//   let hoursBefore = Math.floor(timeBetween % (1000 * 60 * 60 * 24) / (1000 * 60 * 60 ))
+//   let minutesBefore = Math.floor(timeBetween % (1000 * 60 * 60) / (1000 * 60  ))
+//   let secondsBefore = Math.floor(timeBetween % (1000 * 60) / 1000  )
+
+//   timerRefs.daysContent.textContent = daysBefore
+//   timerRefs.hoursContent.textContent = hoursBefore
+//   timerRefs.minsContent.textContent = minutesBefore
+//   timerRefs.secondsContent.textContent = secondsBefore
+// }
+
+// let interval = setInterval(()=>{
+//   getTimeBetween(myDate)
+// }, 1000)
